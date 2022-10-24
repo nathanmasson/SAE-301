386ccecdd8469f5fdfb9b51b99f85a72ab27aec5
Nathan <nathanmassonperso@gmail.com>
2022-10-24 11:53:46 +0200

    Création de la billetterie

--- connect.php
+++ connect.php
@@ -1,14 +1,15 @@
 <?php
-    $serveur = "localhost:3306";
+    $serveur = "localhost";
     $dbname = "sae-301";
     $user = "root";
-    $pass = "root";
+    $pass = "";
     
     try{
         //On se connecte à la BDD
         $dbco = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
         $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
     catch(PDOException $e){
-        echo 'Erreur : '.$e->getMessage();
+        echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
     }
 ?>
--- recup.php
+++ recup.php
@@ -7,4 +7,19 @@
     $mail = $_POST["mail"];
     $tel = $_POST["tel"];
     $mdp = $_POST["mdp"];
+
+    $sqlQuery = 'INSERT INTO `compte-utilisateur`(`Nom`, `Prenom`, `tel`, `Mdp`, `mail`) VALUES (:nom,:prenom,:tel,:mdp,:mail)';
+
+    // Préparation
+    $insertRecipe = $dbco->prepare($sqlQuery);
+
+    // Exécution ! La recette est maintenant en base de données
+    $insertRecipe->execute([
+        'Nom' => $nom,
+        'Prenom' => $prenom,
+        'tel' => $tel,
+        'Mdp' => $mdp,
+        'mail' => $mail,
+    ]);
+        echo "bonjour";
 ?>
